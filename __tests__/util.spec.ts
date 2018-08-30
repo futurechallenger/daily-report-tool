7e0d95ec815c9d5ef3a02b0da9c48cd15a8f4d0e
Uncle Charlie <lixiangdong@mobike.com>
2018-08-31 01:41:33 +0800

    adding tests

--- __tests__/util.spec.ts
+++ __tests__/util.spec.ts
@@ -1,5 +1,22 @@
+import { generatePath } from "../src/util";
+
 describe("example test", async () => {
   test("test", () => {
     expect(2).toEqual(1 + 1);
+  });
+
+  /**
+   * Sample result:
+   * "https://api.github.com/repos/ringcentral/ringcentral-js-widgets/commits?since=2018-8-9T00:00:00Z&until=2018-08-14T:00:00Z"
+   */
+  test("Test generated commit path", () => {
+    expect(
+      generatePath({
+        owner: "ringcentral",
+        repo: "ringcentral-js-widgets",
+        since: new Date().getUTCSeconds(),
+        until: new Date().getUTCSeconds()
+      })
+    ).toEqual();
   });
 });
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,27 @@
+import * as moment from "moment";
+
 export function sleep(ms: number) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
+}
+
+/**
+ *
+ * @param owner: owner of a repo
+ */
+export function generatePath({
+  owner,
+  repo,
+  since,
+  until
+}: {
+  owner: string;
+  repo: string;
+  since: number;
+  until: number;
+}) {
+  const formatedSince = moment.unix(since).format("YYYY-MM-DDTHH:MM:SSZ");
+  const formatedUntil = moment.unix(since).format("YYYY-MM-DDTHH:MM:SSZ");
+  return `https://api.github.com/repos/${owner}/${repo}/commits?since=${formatedSince}&until=${formatedUntil}`;
 }
